2e25eef5409bf196bf00efa8ba7c140501888f7b
Ken S <gxcadr@gmail.com>
2019-10-11 20:29:37 -0700

    added code in background.ts, ui-actions.ts, and eventrecorder.ts

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,48 @@
 // conducts all the action
 // const testBackground: String = 'testin'
 // console.log(testBackground);
+import action from '../actions/ui-actions';
+import { DH_CHECK_P_NOT_PRIME } from 'constants';
 console.log('hey there');
+
+class RecordingController {
+  recording: Array<Object>;
+  badgeStatus: String;
+  bindedMessageHandler: Function;
+  constructor () {
+    this.recording = [];
+    this.badgeStatus = '';
+    this.bindedMessageHandler = null;
+  }
+
+  boot () {
+    chrome.extension.onConnect.addListener(listener => {
+      console.log(listener);
+      listener.onMessage.addListener(msg => {
+        if (msg.action && msg.action === action.START) this.start();
+        if (msg.action && msg.action === action.STOP) this.stop();
+        if (msg.action && msg.action === action.CLEANUP) this.cleanup();
+        if (msg.action && msg.action === action.PAUSE) this.pause();
+        if (msg.action && msg.action === action.UNPAUSE) this.unpause();
+      })
+    }
+  }
+
+  start () {
+    console.log('recording started');
+    this.cleanup(() => {
+      this.badgeStatus = 'rec';
+      this.bindedMessageHandler = this.handleMessage.bind(this);
+
+
+      chrome.runtime.onMessage.addListener(this.bindedMessageHandler);
+    });
+  }
+
+  handleMessage () {
+
+  }
+}
+console.log('booting up the recording controller');
+window.recordingController = new RecordingController();
+window.recordingController.boot();
--- src/event-recording/EventRecorder.ts
+++ src/event-recording/EventRecorder.ts
@@ -1,1 +1,29 @@
 // event recorder
+export default class EventRecorder {
+  constructor() {
+    this.eventLog = []
+  }
+
+  recordEvent(e) {
+    const msg: Object = {
+      action: e.type,
+      value: e.target.value,
+      keycode: e.keycode ? e.keycode : null,
+    };
+    this.sendMessage(msg);
+  }
+
+  sendMessage(msg: Object) {
+    try {
+      if (chrome.runtime && chrome.runtime.onMessage) {
+        chrome.runtime.sendMessage(msg);
+      } else {
+        this.eventLog.push(msg);
+      }
+    }
+    catch (error) {
+      console.debug(error);
+    }
+  }
+  
+}
